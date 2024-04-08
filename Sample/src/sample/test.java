a521710e2093e75aaab569411d19769fef89aa19
dir-24satorei <rei.sato.v322@dirbato.co.jp>
2024-04-08 11:55:38 +0900

    最初のコミット（Sample）

---
+++ Sample/src/sample/test.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class test {
+
+    public static void main(String[] args) {
+        /*
+        System.out.println("kiramex");
+        System.out.println("java");
+        */
+        System.out.println("学習を始めましょう！");
+
+    }
+
+}
